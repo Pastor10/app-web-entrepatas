bf4418a62fb64d89c3a5d22601c1a02e204f8ceb
xlpastorx <luis.pastor@inkafarmadigital.pe>
2020-01-27 14:02:51 -0500

    config setup.sh profile prod

--- setup.sh
+++ setup.sh
@@ -16,7 +16,17 @@ then
 
     echo "Setting up project $PROFILE"
     echo "backend for dev"
-    sed -i -e 's/localhost:9002/f7ws.farmaciasperuanas.pe/g' /var/www/html/fps-app-web-f7/main.js
+    sed -i -e 's/localhost:9002/dev.f7ws.solucionesfps.pe/g' /var/www/html/fps-app-web-f7/main.js
+    echo "filestorage for dev"
+    # sed -i -e 's/35.229.116.96:8585/35.229.116.96:8585/g' /var/www/html/app/main.*.js
+    nginx -g 'daemon off;'
+
+elif [ "$PROFILE" == "QA" ]
+then    
+
+    echo "Setting up project $PROFILE"
+    echo "backend for qa"
+    sed -i -e 's/localhost:9002/qa.f7ws.solucionesfps.pe/g' /var/www/html/fps-app-web-f7/main.js
     echo "filestorage for dev"
     # sed -i -e 's/35.229.116.96:8585/35.229.116.96:8585/g' /var/www/html/app/main.*.js
     nginx -g 'daemon off;'
@@ -26,12 +36,12 @@ then
 
     echo "Setting up project $PROFILE"
     echo "backend for prod"
-    sed -i -e 's/localhost:9002/f7ws.farmaciasperuanas.pe/g' /var/www/html/fps-app-web-f7/main.js
+    #sed -i -e 'htts/localhost:9002/f7ws.farmaciasperuanas.pe/g' /var/www/html/fps-app-web-f7/main.js
+    sed -i -e 's/http:\/\/localhost:9002/https:\/\/f7ws.farmaciasperuanas.pee/g' /var/www/html/fps-app-web-f7/main.js
     echo "filestorage for prod" 
-    # sed -i -e 's/35.229.116.96:8585/35.229.116.96:8585/g' /var/www/html/app/main.*.js
     nginx -g 'daemon off;'
 
 else
-    echo "Please enter a valid PROFILE option [ LOCAL, DEV, PROD ]"
+    echo "Please enter a valid PROFILE option [ LOCAL, DEV, QA, PROD ]"
     nginx -g 'daemon off;'
 fi
