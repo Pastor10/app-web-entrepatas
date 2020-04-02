57afc357d10b8f1ecefebfa23bf86a11df1e5dc5
xlpastorx <luis.pastor@inkafarmadigital.pe>
2020-04-01 22:14:03 -0500

    add msg alert in login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -7,6 +7,7 @@ import {AuthService} from 'src/app/shared/service/auth.service';
 import {LoginResponse} from 'src/app/shared/model/loginResponse.model';
 import {LocalStorageService} from 'src/app/shared/service/localstorage.service';
 import {AppConstant} from 'src/app/shared/constant/app.constant';
+import { UsuarioService } from 'src/app/shared/service/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -19,12 +20,13 @@ export class LoginComponent implements OnInit, AfterViewInit {
   constructor(public router: Router, 
               public messageService: MessageService,
               public authenticationService: AuthService,
-              public localStorageService: LocalStorageService) {
+              public localStorageService: LocalStorageService, public usuarioService: UsuarioService ) {
   }
 
   public username: string;
   public password: string;
   loginResponse: LoginResponse;
+  public state: boolean;
 
   ngOnInit() {
     this.username = '';
@@ -35,8 +37,64 @@ export class LoginComponent implements OnInit, AfterViewInit {
     this.localStorageService.removeItem('userLogin');
   }
 
+
+  private setAutologin(token) {
+    token = token.replace(/[""]+/gi, '');
+    const payload = this.getPayload(token);
+    const pl = JSON.parse(payload.sub);
+    const email = pl.email;
+    //localStorage.setItem('token', token);
+    localStorage.setItem('token', JSON.stringify(token));
+    //this.getUser(email);
+    this.usuarioService.getFindByEmail(email).subscribe(
+      data => {
+      this.state = data.enabled;
+      if(this.state){
+        this.router.navigate(['/main']);
+        } else {
+        
+        this.router.navigate(['/login']);
+        }
+      },
+      error => {
+        const errorMessage =
+          error.message != undefined
+            ? error.message
+            : 'No se pudo procesar la petición';
+        
+      }
+    );
+    return;
+  }
+  getUser(email){
+    this.usuarioService.getFindByEmail(email).subscribe(
+      data => {
+      this.state = data.enabled;
+      },
+      error => {
+        const errorMessage =
+          error.message != undefined
+            ? error.message
+            : 'No se pudo procesar la petición';
+      }
+    );
+  }
+
+  getPayload(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+    atob(base64).split('')
+    .map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join('')
+    );
+    return JSON.parse(jsonPayload);
+    }
+
   public login() {
     if (!this.username || !this.password) {
+      this.showMsg('info', 'Ingrese su usuario y password', 'Usuario');
      // this.alertService.warning('Ingrese su usuario y password');
     } else {
       this.authenticationService.login(this.username, this.password).subscribe(
@@ -50,12 +108,18 @@ export class LoginComponent implements OnInit, AfterViewInit {
         error => {
           console.log('error', error);
           if (error.status == 401) {
-           // this.alertService.warning('Usuario y/o password incorrectos');
+            this.showMsg('error', 'Usuario y/o password incorrectos', 'Usuario');
+           
           } else {
             const errorMessage = error.message != undefined ? error.message : 'No se pudo procesar la petición. Error ' + error.status;
-           // this.alertService.danger(errorMessage);
+            this.showMsg('error', errorMessage, 'Usuario');
+            // this.alertService.danger(errorMessage);
           }
         });
     }
+  }
+
+  showMsg( type: string, msg: string, title: string) {
+    this.messageService.add( { key: 'tst', severity: type, summary: title, detail: msg } );
   }
 }
