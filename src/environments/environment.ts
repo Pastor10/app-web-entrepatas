e584d3416aab1ea230036ec85344982d1bda0d6b
xlpastorx <luis.pastor@inkafarmadigital.pe>
2020-09-03 09:21:56 -0500

    agregando boton aprobar en publicaciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  //END_POINT: 'https://api.entrepatasch.com/',
-  END_POINT: 'http://localhost:9002/',
+  END_POINT: 'https://api.entrepatasch.com/',
+  //END_POINT: 'http://localhost:9002/',
   TIMEOUT: 10000,
   MESSAGE_TIMEOUT: 'Tiempo de respuesta excedido',
   MESSAGE_ERROR_CONNECTION: 'Error al comunicarse con el servidor',
